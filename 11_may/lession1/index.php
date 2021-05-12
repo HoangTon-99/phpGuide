fb10ba7c96f65a20c20f72bfe5ae9b773f56a032
HoangTon_99 <ducton1012@gmail.com>
2021-05-12 07:41:46 +0700

    upload bt Signed-off-by: HoangTon_99 <ducton1012@gmail.com>

--- 11_may/lession1/index.php
+++ 11_may/lession1/index.php
@@ -1,3 +1,17 @@
-<?php
-  echo "Anh tôn đẹp trai";  
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>lession1</title>
+</head>
+<body>
+    <form action="#" method="POST">
+        Số A:<input type="text" name="numberA" >
+        Số B:<input type="text" name="numberB" >
+    <input type="submit" value="+" name="btnsubmit">
+    <button ty>Kết quả</button>
+    </form>
+</body>
+</html>
---
+++ php/bai2.php
@@ -0,0 +1,13 @@
+<?php
+const NAMe = "Hoang Ton";
+define("Welcome", "hello word");
+$x = 5;
+
+$y = 5.6;
+$ten = "Le Duc Ton";
+echo $ten . $x;
+echo "<br>";
+echo NAMe . "<br>" . $y . "<br>" . $ten . "<br>";
+echo Welcome;
+echo NAMe;
+$o = true;
